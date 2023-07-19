b14e2463618e52b2e3ee9f22dba46b27089f2b0e
Amiya Ranjan Das <amiya.ranjan.das@sap.com>
2023-07-20 03:25:35 +0530

    icon bar for instagram added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,25 @@ import logoUrl from "../assets/logo.jpg";
 
 function Header() {
   return (
-    <nav className="navbar navbar-light sticky-top navbar-bg">
-      <a className="navbar-brand title">
-        {" "}
-        <img src={logoUrl} className="d-inline-block align-top header-img" />
-        Artiste Doodles
-      </a>
-    </nav>
+    <>
+      <div className="header-icon-bar">
+        <a href="https://www.instagram.com/artistedoodles.jewellery/">
+          <i className="bi bi-instagram"></i>
+        </a>
+        <a href="#">
+          <i className="bi bi-facebook"></i>
+        </a>
+        <a href="https://wa.me/918100603589">
+          <i className="bi bi-whatsapp"></i>
+        </a>
+      </div>
+      <nav className="navbar navbar-light sticky-top navbar-bg">
+        <a className="navbar-brand title">
+          <img src={logoUrl} className="d-inline-block align-top header-img" />
+          Artiste Doodles
+        </a>
+      </nav>
+    </>
   );
 }
 
